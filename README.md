b6b8d44490476a87dde8b5de808e6c81e1e8b38d
Wilson <wilson61696@hotmail.co.uk>
2024-12-12 12:57:20 +0000

    Added databricks code for querying dataframes using pyspark

--- README.md
+++ README.md
@@ -18,10 +18,18 @@ Mount S3 bucket to databricks
 ## Usage
 Data is retrieved from an external database in `user_posting_emulation.py`, and the function `run_infinite_post_data_loop` is used to emulate data streaming. It selects data from three different tables in a database and sends each to a different kafka topic via a created API.
 
+`s3_cleaning_functions.ipynb` provides functions for loading S3 data into a dataframe via `read_s3`. These dataframes can then be cleaned dependent on the topic via `clean_pin_data`, `clean_geo_data`, and `clean_user_data`.
+
 ## Structure
+
+### Project Structure
 An EC2 instance has Kafka setup, connected to an MSK instance. Three topics `pin`, `geo`, and `user` are created for the three emulated data streams.
 
 -- Diagram of overall structure
+
+
+### Repository Structure
+The `Databricks` directory contains all of the python notebooks used to load data from the MSK S3 bucket, and clean it via `s3_cleaning_functions.ipynb`. `data_querying.ipynb` contains example transformations of the data using pyspark.
 
 
 ## Notes
@@ -32,3 +40,5 @@ Created connector.
 Created API
 
 Connected API to Kafka
+
+Setup databricks to load S3 data, clean, and query.
--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -34,33 +34,32 @@ class AWSDBConnector:
 new_connector = AWSDBConnector()
 
 
-def run_infinite_post_data_loop():
-    while True:
-        sleep(random.randrange(0, 2))
-        random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
+def post_data_loop():
+    sleep(random.randrange(0, 2))
+    random_row = random.randint(0, 11000)
+    engine = new_connector.create_db_connector()
 
-        with engine.connect() as connection:
+    with engine.connect() as connection:
 
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
+        pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+        pin_selected_row = connection.execute(pin_string)
 
-            invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.pin"
-            post_data(invoke_url, pin_selected_row)
+        invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.pin"
+        post_data(invoke_url, pin_selected_row)
             
 
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
+        geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+        geo_selected_row = connection.execute(geo_string)
 
-            invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.geo"
-            post_data(invoke_url, geo_selected_row)
+        invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.geo"
+        post_data(invoke_url, geo_selected_row)
 
 
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
+        user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+        user_selected_row = connection.execute(user_string)
 
-            invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.user"
-            post_data(invoke_url, user_selected_row)
+        invoke_url = "https://804gwh33gk.execute-api.us-east-1.amazonaws.com/dev/topics/0e59bc5e89eb.user"
+        post_data(invoke_url, user_selected_row)
 
 
 def serialize_datetime(obj):
@@ -84,8 +83,9 @@ def post_data(invoke_url, selected_rows):
 
 
 if __name__ == "__main__":
-    run_infinite_post_data_loop()
-    print('Working')
+    while True:
+        post_data_loop()
+        print('Working')
     
     
 
